ad997127c30b2fee02eb19ea6d08094ce72bb816
Asapros <asapros@wp.pl>
2025-03-09 18:05:15 +0100

    loading models and textures

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,35 @@
 
 int main()
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    const int screenWidth = 1000;
+    const int screenHeight = 1000;
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(screenWidth, screenHeight, "kubica");
+    SetExitKey(KEY_ESCAPE);
+    Camera camera = {
+        { 20.0f, 20.0f, 20.0f },
+        { 0.0f, 0.0f, 0.0f },
+        { 0.0f, 1.0f, 0.0f },
+        20.0f,
+        CAMERA_ORTHOGRAPHIC
+    };
+    Model model = LoadModel("../../assets/part.obj");
+    Texture2D texture = LoadTexture("../../assets/texture.jpg");
+    model.materials[0].maps->texture = texture;
+    Vector3 position = { 0.0f, 2.0f, 0.0f };
+    
+    while (!WindowShouldClose())
     {
         BeginDrawing();
+        ClearBackground(GetColor(0x181818ff));
+        BeginMode3D(camera);
 
-            ClearBackground(RAYWHITE);
+        DrawModel(model, {0,0,0}, 1.0f, WHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+        EndMode3D();
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();        // Close window and OpenGL context
-
-    return 0;
+    UnloadModel(model);
+    CloseWindow();
 }
