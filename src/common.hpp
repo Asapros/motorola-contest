b2eba3fbc883e0db9a22efc26890f203bb27a59d
Tomasz Kawalec <tkawalec369@gmail.com>
2025-03-21 12:42:36 +0100

    Some friction-related stuff

--- src/common.hpp
+++ src/common.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
 using EntityId = unsigned int;
+
+#define GRAVITY_ACCELERATION 10.0
--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -1,7 +1,11 @@
-#include "vehicle.hpp"
-#include <raymath.h>
 #include <cmath>
+
+#include <raylib.h>
+#include <raymath.h>
+
+#include "common.hpp"
 #include "rigidbody.hpp"
+#include "vehicle.hpp"
 
 void Vehicle::update() {
     Rigidbody::update();
@@ -12,11 +16,17 @@ void Vehicle::update() {
         float bottom_velocity_rel = wheel.angular_velocity * wheel.radius;
 
         Vector3 velocity = computeVelocity();
-        Vector2 heading_vec{cos(heading), sin(heading)};
+        Vector2 heading_vec{(float)cos(heading), (float)sin(heading)};
         Vector2 bottom_velocity_rel_vec =
             Vector2Scale(heading_vec, bottom_velocity_rel);
 
         Vector2 delta_velocity =
             Vector2Subtract({velocity.x, velocity.y}, bottom_velocity_rel_vec);
+
+        // TODO: determine from tire and ground material
+        float static_friction_coef = 1.0;
+        float kinetic_friction_coef = 1.0;
+
+        float grav_force = mass * GRAVITY_ACCELERATION;
     }
 }
