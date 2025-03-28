196d387c362bdf79499efacd3543609d7dc9afec
Tomasz Kawalec <tkawalec369@gmail.com>
2025-03-28 13:59:31 +0100

    Add virtual destructor to Entity

--- src/entity.cpp
+++ src/entity.cpp
@@ -4,7 +4,9 @@
 #include "world.hpp"
 
 Entity::Entity(std::shared_ptr<ModelWrapper> model, Vector3 position)
-    : world(nullptr), eid(0), model(model), position(position) {};
+    : world(nullptr), eid(0), model(model), position(position) {}
+
+Entity::~Entity() {}
 
 void Entity::update([[maybe_unused]] float delta_time) {}
 
--- src/entity.hpp
+++ src/entity.hpp
@@ -18,6 +18,7 @@ class Entity {
     Vector3 position;
 
     Entity(std::shared_ptr<ModelWrapper> model, Vector3 position);
+    virtual ~Entity();
 
     virtual void update(float delta_time);
     virtual void draw();
