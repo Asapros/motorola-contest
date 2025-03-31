b5f921dce23cf883f13e1e479212f8f607ee0434
Tomasz Kawalec <tkawalec369@gmail.com>
2025-03-31 23:10:12 +0200

    Some changes to AI - still doesn't work as expected

--- src/ai.cpp
+++ src/ai.cpp
@@ -1,29 +1,30 @@
 #include "ai.hpp"
-#include "raylib.h"
-#include "world.hpp"
-#include "vehicle.hpp"
+#include <cmath>
 #include "common.hpp"
 #include "debug.hpp"
-#include <cmath>
+#include "raylib.h"
+#include "vehicle.hpp"
+#include "world.hpp"
 
 Controls followPoint(Vector2 point, Vector2 position, float orientation) {
-    float accelerator = 1;
+    float accelerator = 0.2;
     float steering = 0.0;
 
-    float dx = position.x - point.x;
-    float dy = position.y - point.y;
+    float dx = point.x - position.x;
+    float dy = point.y - position.y;
 
-    float desiredAngle = atan2(dy, dx);
+    float desiredAngle = -atan2(dy, dx);
 
-    debugValues["ai_y_del"] = std::to_string(position.y-point.y);
-    debugValues["ai_x_del"] = std::to_string(position.x-point.x);
+    debugValues["ai_y_del"] = std::to_string(dy);
+    debugValues["ai_x_del"] = std::to_string(dx);
 
-    steering = (desiredAngle - orientation);
+    steering = (desiredAngle - orientation) * 0.5;
     debugValues["ai_desired_angle"] = std::to_string(desiredAngle);
     return Controls{accelerator, steering, 5};
 }
 
 Controls StraightGuy::computeControls(World* world, EntityId aiId) {
     auto player = dynamic_cast<Vehicle*>(world->entities[aiId].get());
-    return followPoint({0, 0}, Vector2{player->position.x, player->position.z}, player->heading);
+    return followPoint({0, 0}, Vector2{player->position.x, player->position.z},
+                       player->heading);
 }
