3b659d1b3e4c7a087deaaa8834cb9d71cd17aec7
Asapros <asapros@wp.pl>
2025-03-31 22:26:13 +0200

    Start making AI

--- src/ai.cpp
+++ src/ai.cpp
@@ -1,10 +1,29 @@
 #include "ai.hpp"
 #include "raylib.h"
 #include "world.hpp"
+#include "vehicle.hpp"
 #include "common.hpp"
+#include "debug.hpp"
+#include <cmath>
+
+Controls followPoint(Vector2 point, Vector2 position, float orientation) {
+    float accelerator = 1;
+    float steering = 0.0;
+
+    float dx = position.x - point.x;
+    float dy = position.y - point.y;
+
+    float desiredAngle = atan2(dy, dx);
+
+    debugValues["ai_y_del"] = std::to_string(position.y-point.y);
+    debugValues["ai_x_del"] = std::to_string(position.x-point.x);
+
+    steering = (desiredAngle - orientation);
+    debugValues["ai_desired_angle"] = std::to_string(desiredAngle);
+    return Controls{accelerator, steering, 5};
+}
 
 Controls StraightGuy::computeControls(World* world, EntityId aiId) {
-    float accelerator = 1.0;
-    float steering = 0.2;
-    return Controls{accelerator, steering, 5};
+    auto player = dynamic_cast<Vehicle*>(world->entities[aiId].get());
+    return followPoint({0, 0}, Vector2{player->position.x, player->position.z}, player->heading);
 }
